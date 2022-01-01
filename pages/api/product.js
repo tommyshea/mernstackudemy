222742d1747261ef7b6edc7ca531ecb7b30d6dee
tommyshea <tommy.shea@gmail.com>
2022-01-01 16:22:02 -0500

    updated as of vid #20

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,9 +1,31 @@
 import Product from '../../models/Product'
+import connectDb from '../../utils/connectDb'
+
+connectDb()
 
 const handleGetRequest = async (req, res) => {
     const { _id } = req.query
     const product = await Product.findOne({ _id: _id})
     res.status(200).json(product)
+}
+
+const handlePostRequest = async (req, res) => {
+    const { name, price, description, mediaUrl } = req.body
+    try {
+        if (!name || !price || !description || !mediaUrl) {
+            return res.status(422).send("Product missing one or more fields.")
+        }
+        const product = await new Product({
+            name,
+            price,
+            description,
+            mediaUrl
+        }).save()
+        res.status(201).json(product)
+    } catch(error) {
+        console.error(error)
+        res.status(500).json("Server error in creating product: ", error.message)
+    }
 }
 
 const handleDeleteRequest = async (req, res) => {
@@ -16,6 +38,9 @@ export default async (req, res) => {
     switch(req.method) {
         case "GET": 
             await handleGetRequest(req, res)
+            break
+        case "POST":
+            await handlePostRequest(req, res)
             break
         case "DELETE":
             await handleDeleteRequest(req, res)
--- pages/create.js
+++ pages/create.js
@@ -1,5 +1,164 @@
-function CreateProduct() {
-  return <>create</>;
+import React, { useState, useEffect } from 'react'
+import {
+  Form,
+  Input,
+  TextArea,
+  Button,
+  Image,
+  Message,
+  Header,
+  Icon,
+} from "semantic-ui-react";
+import axios from 'axios'
+import baseUrl from '../utils/baseUrl'
+import catchErrors from '../utils/catchErrors';
+
+const INITIAL_PRODUCT = {
+  name: "", 
+  price: "",
+  media: "",
+  description: ""
 }
+
+const CreateProduct = () => {
+  const [product, setProduct] = useState(INITIAL_PRODUCT)
+  const [mediaPreview, setMediaPreview] = useState('')
+  const [success, setSuccess] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [disabled, setDisabled] = useState(true)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    const isProduct = Object.values(product).every(el => Boolean(el))
+    isProduct ? setDisabled(false) : setDisabled(true)
+  }, [product])
+  
+  const handleChange = (e) => {
+    const { name, value, files } = e.target
+    if (name === 'media') {
+      setProduct((prevState) => ({
+        ...prevState,
+        media: files[0]
+      }))
+      setMediaPreview(window.URL.createObjectURL(files[0]))
+    } else {
+      setProduct((prevState) => ({ 
+        ...prevState,
+        [name]: value 
+      }))
+    }
+    //console.log(product)
+  }
+
+  const handleImageUpload = async () => {
+    const data = new FormData()
+    data.append('file', product.media)
+    data.append('upload_preset', 'reactreserve')
+    data.append('cloud_name', 'ds0pxodai')
+    const response = await axios.post(process.env.CLOUDINARY_URL, data)
+    const mediaUrl = response.data.url
+    return mediaUrl
+  }
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault()
+      setLoading(true)
+      const mediaUrl = await handleImageUpload()
+      const url = `${baseUrl}/api/product`
+      const { name, price, description } = product
+      const payload = { name, price, description, mediaUrl }
+      const response = await axios.post(url, payload)
+      setProduct(INITIAL_PRODUCT)
+      setSuccess(true)
+    } catch(error) {
+      catchErrors(error, setError)
+      // console.error("ERROR: ", error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <>
+      <Header as="h2" block>
+        <Icon name="add" color="orange" />
+        Create New Product
+      </Header>
+      <Form 
+        success={success} 
+        loading={loading} 
+        error={Boolean(error)}
+        onSubmit={handleSubmit}
+      >
+        <Message 
+          error
+          // icon="check"
+          header="Oops!"
+          content={error}
+        />
+        <Message 
+          success
+          icon="check"
+          header="Success!"
+          content="Your product has been posted!"
+        />
+        <Form.Group widths="equal">
+          <Form.Field 
+            control={Input}
+            name="name"
+            label="Name"
+            placeholder="Name"
+            type="text"
+            value={product.name}
+            onChange={handleChange}
+          />
+          <Form.Field 
+            control={Input}
+            name="price"
+            label="Price"
+            placeholder="Price"
+            min="0.00"
+            step="0.01"
+            type="number"
+            value={product.price}
+            onChange={handleChange}
+          />
+          <Form.Field 
+            control={Input}
+            name="media"
+            label="Media"
+            type="file"
+            accept="image/*"
+            content="Select Image"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Image 
+          src={mediaPreview}
+          rounded
+          centered
+          size="small"
+        />
+        <Form.Field 
+          control={TextArea}
+          name="description"
+          label="Description"
+          placeholder="Description"
+          value={product.description}
+          onChange={handleChange}
+        />
+        <Form.Field 
+          control={Button}
+          color="blue"
+          icon="pencil alternate"
+          content="Submit"
+          type="submit"
+          disabled={disabled || loading}
+        />
+      </Form>
+    </>
+  )
+};
 
 export default CreateProduct;
